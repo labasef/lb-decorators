764f7db5fbe1775775cb5228671628052622329b
labase <github@labase.dev>
2022-10-21 10:55:27 +0200

    new decorator register

--- src/decorators/misc_tools.py
+++ src/decorators/misc_tools.py
@@ -2,6 +2,20 @@ import functools as ft
 from collections.abc import Callable
 
 
+def register(func=None, reg: dict = None):
+    """Register a function in a function dictionary"""
+    if reg is None:
+        reg = {}
+    if func:
+        reg[func.__name__] = func
+    else:
+        @ft.wraps(func)
+        def wrapper(func):
+            reg[func.__name__] = func
+            return func
+        return wrapper
+
+
 def repeat(_func=None, *, num_times: (int, Callable[[], int]) = 2):
     """decorator used to repeat the execution of a function as specified by num_times
     :arg num_times: the number of repetitions (default=2)"""
